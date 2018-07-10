6fb8df980952ee2b14c2ae102671692c640da57e
trycatchlearn <trycatchlearn@outlook.com>
2018-07-10 11:34:15 +0100

    End of section 16

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -11,9 +11,10 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent},
+    { path: '', component: HomeComponent},
     {
         path: '',
         runGuardsAndResolvers: 'always',
@@ -24,9 +25,9 @@ export const appRoutes: Routes = [
                 resolve: {user: MemberDetailResolver}},
             { path: 'member/edit', component: MemberEditComponent,
                 resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges]},
-            { path: 'messages', component: MessagesComponent},
+            { path: 'messages', component: MessagesComponent, resolve: {messages: MessagesResolver}},
             { path: 'lists', component: ListsComponent, resolve: {users: ListsResolver}},
         ]
     },
-    { path: '**', redirectTo: 'home', pathMatch: 'full'},
+    { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
